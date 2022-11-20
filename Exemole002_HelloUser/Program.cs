c15e9bbbf22da00ef0e8f562eda69e874036be2a
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-11-21 01:18:23 +0500

    вывод текста в разных строках

--- Exemole002_HelloUser/Program.cs
+++ Exemole002_HelloUser/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, USER!");
-
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
