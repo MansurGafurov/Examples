3b12ef6d24322624755abdb53116965f0cdd24c9
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-11-21 18:05:45 +0500

    add program  sum random numbers

---
+++ Exemple005_RandomNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberA = new Random().Next(1, 20);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 20);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
