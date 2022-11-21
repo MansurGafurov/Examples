c2ca6e794a2f1b90c7f3c02becd271ad8391aad9
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-11-21 20:18:09 +0500

    add program  If & Else

---
+++ Exemple006_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите свое имя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "мансур")
+{
+    Console.WriteLine("Здравствуй дорогой");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
