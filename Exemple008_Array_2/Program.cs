3d0b95f182b37c83bb58bd8ab68636a3a907cd04
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-11-24 03:05:48 +0500

    add progrem max number use METHOD & ARRAY_2

---
+++ Exemple008_Array_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// index          0   1   2  3   4  5   6  7  8
+int [] array = { 11, 26, 33, 4, 33, 6, 8, 88, 33 };
+
+int n = array.Length;
+int index = 0;
+int find = 33;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine( " Index = " + index );
+        break; 
+    }
+    index ++;
+    // или index += 1
+    // или index = index + 1
+}
