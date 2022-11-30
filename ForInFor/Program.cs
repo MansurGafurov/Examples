685bb938ae9df24a5f10e8cabcef5154f2a4cc82
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-11-30 16:34:58 +0500

    разобрал цикл в цикле. таблицы умножения

---
+++ ForInFor/Program.cs
@@ -0,0 +1,9 @@
+﻿// Цикл в цикле! таблица умножения!
+
+for(int i = 1; i < 10; i++){
+    for( int y = 1; y < 10; y++){
+        Console.WriteLine($"{i} * {y} = {i * y}");
+    }
+    Console.WriteLine();
+}
+
