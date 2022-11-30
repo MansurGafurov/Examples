841a9535b8978193fa2348d741c106d0416cef11
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-11-30 16:17:03 +0500

    разобрал виды методов Exemple009

--- Exemple007_Method/Program.cs
+++ Exemple007_Method/Program.cs
@@ -19,12 +19,13 @@ int b3 = 87;
 int c3 = 31;
 
 // это более длинная версия кода 
+
+
+
 //int max1 = Max(a1, b1, c1);
 //int max2 = Max(a2, b2, c2);
 //int max3 = Max(a3, b3, c3);
 //int max = Max(max1, max2, max3);
-
-
 //Это более короткий вариант кода.
 int max = Max(Max(a1,b1,c1), Max(a2,b2,c2), Max(a3,b3,c3));
 
---
+++ Exemple009_vid_mathods/Program.cs
@@ -0,0 +1,60 @@
+﻿// Виды методов!
+
+// Вид1 - ничего не возвращают, ничего не принимают!
+
+void Method1 () {
+    Console.WriteLine("Автор: Гафуров Мансур!");
+}
+// Method1(); - вызвать метод1
+
+
+
+
+//Вид2 - Ничего не возвращают, но принимают аргументы!
+void Method2 (string msg) {
+    Console.WriteLine(msg);
+}
+//Method2("Hi, Mansur!"); - вызвать метод2
+
+
+
+
+
+void Method21 (string msg, int count) {
+    for(int i = 0; i < count; i++) Console.WriteLine(msg);
+}
+// Method21("Mansur", 5); - вызвать метод2.1
+
+
+
+
+
+
+// Вид 3 - Что-то возвращ, но ничего не принимает!
+
+int Method3() {
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+// Console.WriteLine(year);
+
+
+
+
+
+
+
+// Вид 4 - Что то принимает и что то возвращ.
+
+string Method4(int count, string text) {
+    string result = string.Empty;
+    for(int i = 0; i < count; i++) 
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(6, "Mansur");
+Console.WriteLine(res);
