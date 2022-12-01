bb493016f14ff087c6a2e74a78f05a97de399b60
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-01 11:26:17 +0500

    работа с текстом2

---
+++ Exemple010_РаботаНадТекстом/Program.cs
@@ -0,0 +1,29 @@
+﻿// В тексте необходимо все пробелы заменить на черточки, а маленькие буквы к на большие буквы К
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace (string text, char oldValue, char newValue) 
+{
+    string result = string.Empty;
+    int length = text.Length;
+
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+            
+    }  
+    return result;
+}
+
+string NewText  = Replace(text, 'к', 'К');
+Console.WriteLine(NewText);
+Console.WriteLine();
+NewText  = Replace(NewText, ' ', '|');
+Console.WriteLine(NewText);
+Console.WriteLine();
+NewText  = Replace(NewText, 'а', '@');
+Console.WriteLine(NewText);
