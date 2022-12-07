1e753d9fc07cb011abad2a176a58d2f33b37609a
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-07 12:36:55 +0500

    двумерный массив

---
+++ Exemple011_МАССИВ_МЕТОД_УПОРЯДОЧИТЬ/Program.cs
@@ -0,0 +1,6 @@
+﻿// ЗАДАЧА. Упорядочить(отсартировать) данные внутри массива!
+
+ int [] array = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+ void PrintArray (int [] arr)
+Console.WriteLine("Hello, World!");
---
+++ ДВУМЕРНЫЙ_МАССИВ_1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+void PrintArray (int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int y = 0; y < matr.GetLength(1); y++)
+        {
+            matr[i, y] += 1; 
+            Console.Write($"{matr[i, y]} ");
+        }
+    Console.WriteLine();    
+    }
+}
+
+void FullArray (int[,] matr)
+{
+    for(int x = 0; x < matr.GetLength(0); x++)
+    {
+        for(int y = 0; y < matr.GetLength(1); y++)
+        {
+            matr[x, y] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+
+
+int[,] matrix = new int[3, 6];
+PrintArray(matrix);
+Console.WriteLine();
+FullArray(matrix);
+PrintArray(matrix);
+
+
+// string[,] table = new string[2, 4];
+// for(int rows = 0; rows < table.GetLength(0); rows++)
+// {
+//     for(int columns = 0; columns < table.GetLength(1); columns++)
+//     {
+//         table[rows, columns] = "слово";
+//         Console.Write($"{table[rows, columns]} ");
+//     }
+// Console.WriteLine();    
+// }
