0edf6574132bdcbc5d8055722db24bfd8c64f14b
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-10 20:35:34 +0500

    add Fibo

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,10 @@
+﻿int Fibocci (int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibocci(n - 1) + Fibocci(n - 2);
+}
+for(int i = 1; i < 30; i++)
+{
+    Console.WriteLine(Fibocci(i));
+}
+
